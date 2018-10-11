111d0def4ca2f4f2c221d4826a33eb133e99a1e6
evantjuh <hardpop_radio@hotmail.com>
2018-10-11 14:03:34 +0200

    Final changes

--- Game Jam/Assets/Scripts/Player.cs
+++ Game Jam/Assets/Scripts/Player.cs
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour {
 
     public List<GameObject> items { get; set; }
 
-    public int itemsAmount = 4;
+    public int itemsAmount = 6;
 
     [SerializeField]
     private int currentObjectiveNumber = 0;
@@ -45,7 +45,7 @@ public class Player : MonoBehaviour {
 
     public void AddItem(GameObject item)
     {
-        if (!this.items.Contains(item) && items.Count <= itemsAmount)
+        if (!this.items.Contains(item))
         {
             this.items.Add(item);
             ItemUIController itemUIController = GameObject.FindObjectOfType<ItemUIController>();
@@ -53,6 +53,10 @@ public class Player : MonoBehaviour {
 
             HandlePickup(item);
 
+        } else
+        {
+            Debug.Log(!this.items.Contains(item));
+            Debug.Log(items.Count <= itemsAmount);
         }
     }
 
@@ -88,9 +92,9 @@ public class Player : MonoBehaviour {
         if(!foundRedKey && !foundGreenKey && !hasKeys && startLookingForCodes)
         {
             if (hasCodeOne && !hasCodeTwo)
-                objectiveUIController.SetText("Find the Cup!");
+                objectiveUIController.SetText("Find Code One!");
             else if (!hasCodeOne && hasCodeTwo)
-                objectiveUIController.SetText("Find the Guitar!");
+                objectiveUIController.SetText("Find Code Two!");
             else if (hasCodeOne && hasCodeTwo)
             {
                 objectiveUIController.SetText("Get to the exit!");
