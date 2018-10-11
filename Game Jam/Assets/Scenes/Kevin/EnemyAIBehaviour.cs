4c604b554046d9b559b040db9d460433ecfc482c
axestos <kevin.w@live.nl>
2018-10-11 12:05:05 +0200

    Foutje in mijn code voor Commit

--- Game Jam/Assets/Scenes/Kevin/EnemyAIBehaviour.cs
+++ Game Jam/Assets/Scenes/Kevin/EnemyAIBehaviour.cs
@@ -14,21 +14,37 @@ public class EnemyAIBehaviour : MonoBehaviour {
 
 	public FSMState curState;
 	public float curSpeed;
+	private GameObject player;
 
-	protected override void Initialize(){
-		curState = FSMState.Patrol;
-	
+	void Initialize(){
+		curState = FSMState.Patrol;	
 	}
-
-
 
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("Player");	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		switch (curState) 
+		{
+		case FSMState.Patrol:
+			UpdatePatrolState ();
+			break;
+		case FSMState.Chase:
+			UpdateChaseState ();
+			break;
+		}
+	}
+
+	void UpdatePatrolState(){
+
+	}
+
+	void UpdateChaseState(){
 		
 	}
+
 }
